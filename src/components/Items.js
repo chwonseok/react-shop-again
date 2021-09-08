669225dab50bf4c3c2a2666772606570a7fb7873
cws0316 <dev.chwonseok@gmail.com>
2021-09-08 23:59:23 +0900

    minor change

--- src/components/Items.js
+++ src/components/Items.js
@@ -5,17 +5,29 @@ function Items() {
     <div className="items">
       <div className="row">
         <div className="col">
-          <img className="bike-1" alt="madone" />
+          <img
+            className="bike-1"
+            src="https://d2yn9m4p3q9iyv.cloudfront.net/trek/2021/madone-slr-6-etap/thumbs/1000/b2e0e.jpeg"
+            alt="madone"
+          />
           <h4>Madone</h4>
           <p>TREK, ₩1,000,000</p>
         </div>
         <div className="col">
-          <img className="bike-2" src={'../img/madone.webp'} alt="xr4" />
+          <img
+            className="bike-2"
+            src="https://www.bianchi.sk/uploads/image_bank/38670-cestny-karbonovy-bicykel-bianchi-oltre-xr4-disc-super-record-eps-12sp-52-36-v.jpg"
+            alt="xr4"
+          />
           <h4>XR4</h4>
           <p>BIANCHI, ₩1,000,000</p>
         </div>
         <div className="col">
-          <img className="bike-3" alt="dogma" />
+          <img
+            className="bike-3"
+            src="https://cdn.shopify.com/s/files/1/0008/9738/4508/products/veniceblue_1000x1000.png?v=1600961656"
+            alt="dogma"
+          />
           <h4>Dogma</h4>
           <p>PINARELLO, ₩1,000,000</p>
         </div>
